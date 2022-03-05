287461f39d8e82bc6f02328e09852be907d81119
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-05 22:15:13 +0400

    add Error Handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,28 +1,41 @@
+
+
+
+
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
-const getGoals = (req,res)=> {
+const getGoals = async (req,res)=> {
+    console.log(req.body)
     res.status(200).json({"message":"Get goals"})
+    
 }
 
 // @desc Set goal
 // @route POST /api/goals
 // @access Private
-const setGoal = (req,res) => {
+const setGoal = async (req,res) => {
+    if(!req.body.text) {
+        // res.status(400).json({message:"Please add a text field"})
+        res.status(400)
+        throw new Error('Please add a text field') // ERROR handling
+        
+    }
     res.status(200).json({"message":"Set goal"})
+    
 }
 
 // @desc Update goal
 // @route PUT /api/goals/:id
 // @access Private
-const updateGoal = (req,res)=>{
+const updateGoal = async (req,res)=>{
     res.status(200).json({"message":`Update goal ${req.params.id}`})
 }
 
 // @desc Delete goal
 // @route DELETE /api/goals/:id
 // @access Private
-const deleteGoal = (req,res) => {
+const deleteGoal = async (req,res) => {
     res.status(200).json({"message":`Delete goal ${req.params.id}`})
 }
 
