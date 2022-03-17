da0a3365f3a751b71a73f7b50e8a7d16f2f5db71
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-17 13:25:14 +0400

    set Register page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,98 @@
+import { useState, useEffect } from "react";
+import { FaUser } from "react-icons/fa";
+
 function Login() {
+  // create formData object using useState
+  const [formData, setFormData] = useState({
+    name:'',
+    email:'',
+    password:'',
+    password2:'',
+  });
+
+  // destructure formData object
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => (
+        {
+            ...prevState,
+            [e.target.name] : e.target.value,
+            
+        }
+    ))
+    
+  };
+
+  const onSubmit = (e) => {
+      e.preventDefault();
+  }
+
   return (
-    <div>Login</div>
-  )
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Hello
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Enter your name"
+              className="form-control"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              className="form-control"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter your password"
+              className="form-control"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <input
+              type="password"
+              id="password2"
+              name="password2"
+              value={password2}
+              placeholder="Confirm your password"
+              className="form-control"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+              <button type="submit" className="btn btn-block">
+                  Submit
+              </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Login
+export default Login;
