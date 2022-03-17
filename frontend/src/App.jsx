a20f69cc4b6d696fb4e40cdd62021d5eff3910ab
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-17 12:35:24 +0400

    set Header and navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import "./index.css";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
@@ -8,6 +9,7 @@ function App() {
     <>
       <Router>
         <div className="container">
+          <Header />
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,26 @@
-import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div>
-      Header
-    </div>
-  )
+    <header className="header">
+      <div className="logo">
+        <Link to="/">GoalSetter</Link>
+      </div>
+      <ul>
+          <li>
+              <Link to="/login">
+                  <FaSignInAlt />Login
+              </Link>
+          </li>
+          <li>
+              <Link to="/register">
+                  <FaUser />Register
+              </Link>
+          </li>
+      </ul>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
