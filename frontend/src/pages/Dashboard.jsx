463b746efeeb52d81e756ee573fc925b6c3dbe0d
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-18 23:55:28 +0400

    feat: inform about goal number

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,37 +1,38 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import {useSelector, useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 import GoalForm from "../components/GoalForm";
-import Spinner from "../components/Spinner"
-import {reset, getGoals} from "../features/goals/goalSlice"
-import GoalItem from "../components/GoalItem"
+import Spinner from "../components/Spinner";
+import { reset, getGoals } from "../features/goals/goalSlice";
+import GoalItem from "../components/GoalItem";
 
 function Dashboard() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
-  const {goals, isError, isLoading, message} = useSelector((state) => state.goals) 
-
+  const { goals, isError, isLoading, message } = useSelector(
+    (state) => state.goals
+  );
 
   useEffect(() => {
-    if(isError) {
-      console.log(message)
+    if (isError) {
+      console.log(message);
     }
 
     if (!user) {
       navigate("/login");
     }
 
-    dispatch(getGoals())
+    dispatch(getGoals());
 
     return () => {
-      dispatch(reset())
-    }
+      dispatch(reset());
+    };
   }, [user, navigate, isError, dispatch, message]);
 
-  if(isLoading) {
-    return <Spinner />
+  if (isLoading) {
+    return <Spinner />;
   }
 
   return (
@@ -41,19 +42,28 @@ function Dashboard() {
         <p>Goals Dashboard</p>
       </section>
       <GoalForm />
-      <section className="content">
-        {goals.length > 0 
-        ? (
+      {goals.length >= 5 ? (
+        <>
+          <h3 id="red"> You have set too many goals</h3>
           <div className="goals">
             {goals.map((goal) => (
               <GoalItem key={goal._id} goal={goal} />
             ))}
           </div>
-        ) 
-        :(
-          <h3> You have not set any goals</h3>
-        )}
-      </section>
+        </>
+      ) : (
+        <section className="content">
+          {goals.length > 0 ? (
+            <div className="goals">
+              {goals.map((goal) => (
+                <GoalItem key={goal._id} goal={goal} />
+              ))}
+            </div>
+          ) : (
+            <h3> You have not set any goals</h3>
+          )}
+        </section>
+      )}
     </>
   );
 }
