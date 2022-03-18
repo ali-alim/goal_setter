91a5da28a5a9cca0fa55364a703a17e2e7be21bf
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-18 20:02:52 +0400

    feat: dashboard and goal form

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,23 +1,29 @@
-import {useEffect} from 'react'
-import {useNavigate} from "react-router-dom"
-import {useSelector} from 'react-redux'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import GoalForm from "../components/GoalForm";
 
 function Dashboard() {
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
 
-  const navigate = useNavigate()
-  const {user} = useSelector((state) => state.auth)
 
-  useEffect(()=>{
-    if(!user) {
-      navigate('/login')
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
     }
-  }, [user, navigate])
+  }, [user, navigate]);
+
 
   return (
-    <div>
-      Dashboard
-    </div>
-  )
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+      <GoalForm />
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
