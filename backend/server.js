ce73cfd683adade844b98e448cf3e93c4071e0b4
ali-alim <ali.mamedov.2016@gmail.com>
2022-03-19 00:38:19 +0400

    feat: heroku setup and adding env variables to config vars

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,32 @@
-const express = require("express")
-const dotenv = require("dotenv").config()
-const colors = require('colors')
-const { errorHandler } = require("./middleware/errorMiddleware")
-const connectDB = require("./config/db")
-const port = process.env.PORT || 5000
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 5000;
 
+connectDB();
+const app = express();
 
-connectDB()
-const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
-app.use('/api/goals', require('./routes/goalRoutes'))
-app.use('/api/users', require('./routes/userRoutes'))
+//server frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build"))); // this will be our static folder
 
-app.use(errorHandler)
+  app.get('*', (req, res) =>
+    res.sendFile(
+        path.resolve(__dirname, "../", "frontend", "build", "index.html"))
+  );
+} else {
+    app.get('/', (req,res) => res.send('Please set your production'))
+}
 
-app.listen(port, ()=>console.log(`server started on port ${port}`))
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`server started on port ${port}`));
